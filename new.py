adcd896eb11583863f41615d63cb21d4fc8a0382
Subramani <subramm@global.anz.com>
2018-09-07 09:54:31 +0530

    selenium new

--- new.py
+++ new.py
@@ -1,9 +1,19 @@
-a =10 
-b =20
-c =30
-d =60
-e =60
-print(a+b+c+d+e)
-print(a)
+import selenium
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+import win32com.client as win32
+import logging
+from datetime import datetime
+import datetime
+                        
+executable_path = 'C:/NotBackedUp/ChromeDriver/chromedriver.exe'
+capabilities = { 'chromeOptions':  { 'useAutomationExtension': False}}
+driver = webdriver.Chrome(executable_path,desired_capabilities = capabilities)
 
 
+driver.get("https:\\Google.com")
+time.sleep(20)
+
+driver.get("https://www.udemy.com/java-tutorial/learn/v4/t/lecture/131596?start=0")
+time.sleep(20)
